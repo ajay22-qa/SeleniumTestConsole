cfbebe0a7c475ad2013301fba8854500d1b19730
ajay22-qa <32628262+ajay9761@users.noreply.github.com>
2021-03-26 13:43:21 +0530

    copy1

--- SeleniumTestConsole/SeleniumTestConsole/Sample1.cs
+++ SeleniumTestConsole/SeleniumTestConsole/Sample1.cs
@@ -9,7 +9,8 @@ using System.Threading;
 using NUnit.Framework;
 //using System.Text;
 //using System.Threading.Tasks;
-
+//using System.Text;
+//using System.Threading.Tasks;
 namespace SeleniumTest
 {
     class Sample1
